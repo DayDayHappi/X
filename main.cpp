5ca08c94463f4f583ef7d815dcf72b116c1e1826
DayDayHappi <1824661791@qq.com>
2024-11-10 11:12:56 +0800

    second commit

--- main.cpp
+++ main.cpp
@@ -9,8 +9,6 @@ int main()
     string a = "abcdefghigk";
     cout << sizeof(a) << endl;
     cout << "sizeof(string) =" <<sizeof(string) << endl;
-    char * p;
-    cout << "sizeof(p)" << sizeof(p) << endl;
     delete obj;
     return 0;
 }
