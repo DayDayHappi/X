f03fe7865cd4681d30d0fef93bb32edf1afcd6ce
DayDayHappi <1824661791@qq.com>
2024-11-10 11:48:12 +0800

    3rd

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ int main()
     string a = "abcdefghigk";
     cout << sizeof(a) << endl;
     cout << "sizeof(string) =" <<sizeof(string) << endl;
+    char * p;
+    cout << "sizeof(p)" << sizeof(p) << endl;
     delete obj;
     return 0;
 }
